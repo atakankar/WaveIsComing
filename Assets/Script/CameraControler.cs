ea06113717da5dbf25b77b04c1cd43149a2771fe
Atakan Kar <atkan_kar55@hotmail.com>
2021-02-04 02:30:27 +0300

    v0.6

--- Assets/Script/CameraControler.cs
+++ Assets/Script/CameraControler.cs
@@ -13,6 +13,20 @@ public class CameraControler : MonoBehaviour
     public float minY = 10f;
     public float maxY = 80f;
 
+
+    float MouseZoomSpeed = 15.0f;
+    float TouchZoomSpeed = 0.1f;
+    float ZoomMinBound = 10f;
+    float ZoomMaxBound = 80f;
+    float scroll =0;
+    Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -28,22 +42,22 @@ public class CameraControler : MonoBehaviour
         if (!doMovement)
             return;
         // w s a d tuşları ve hareketleri
-        if(Input.GetKey("w") /*|| Input.mousePosition.y >= Screen.height - panBorderThickness*/)
+        if(Input.GetKey("w") )
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
         }
 
-        if (Input.GetKey("s") /*|| Input.mousePosition.y <= Screen.height - panBorderThickness*/)
+        if (Input.GetKey("s") )
         {
             transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
         }
 
-        if (Input.GetKey("d") /*|| Input.mousePosition.x >= Screen.width - panBorderThickness*/)
+        if (Input.GetKey("d"))
         {
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
         }
 
-        if (Input.GetKey("a") /*|| Input.mousePosition.x <= Screen.height - panBorderThickness*/)
+        if (Input.GetKey("a") )
         {
             transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
@@ -55,6 +69,45 @@ public class CameraControler : MonoBehaviour
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
         transform.position = pos;
 
+        if (Input.touchSupported)
+        {
+            // Pinch to zoom
+            if (Input.touchCount == 2)
+            {
 
+                // get current touch positions
+                Touch tZero = Input.GetTouch(0);
+                Touch tOne = Input.GetTouch(1);
+                // get touch position from the previous frame
+                Vector2 tZeroPrevious = tZero.position - tZero.deltaPosition;
+                Vector2 tOnePrevious = tOne.position - tOne.deltaPosition;
+
+                float oldTouchDistance = Vector2.Distance(tZeroPrevious, tOnePrevious);
+                float currentTouchDistance = Vector2.Distance(tZero.position, tOne.position);
+
+                // get offset value
+                float deltaDistance = oldTouchDistance - currentTouchDistance;
+                Zoom(deltaDistance, TouchZoomSpeed);
+            }
+        }
+
+        if (cam.fieldOfView < ZoomMinBound)
+        {
+            cam.fieldOfView = 0.1f;
+        }
+        else
+        if (cam.fieldOfView > ZoomMaxBound)
+        {
+            cam.fieldOfView = 179.9f;
+        }
+    }   
+
+
+    void Zoom(float deltaMagnitudeDiff, float speed)
+    {
+
+        cam.fieldOfView += deltaMagnitudeDiff * speed;
+        // set min and max value of Clamp function upon your requirement
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, ZoomMinBound, ZoomMaxBound);
     }
 }
--- Assets/Script/CompleteLevel.cs
+++ Assets/Script/CompleteLevel.cs
@@ -18,8 +18,8 @@ public class CompleteLevel : MonoBehaviour
     public SceneFader sceneFader;
 
 
-    public string nextLevel = "Level02";
-    public int levelToUnlock = 2;
+    public string nextLevel ;
+    public int levelToUnlock ;
 
     void Awake()
     {
